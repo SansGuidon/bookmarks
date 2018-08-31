33b52e829323c8c8a9b6d81970fd71a8d9d96683
Morgan Wattiez <mwattiez@isabel.eu>
2018-08-31 11:41:46 +0200

    script: get rid of repos names in authors

--- top_authors.sh
+++ top_authors.sh
@@ -1,1 +1,1 @@
-grep http README.md | cut -d '[' -f 2 | cut -d ']' -f 1 | sed 's/\*//g' | sort | uniq -c | sort -n
+grep http README.md | cut -d '[' -f 2 | cut -d ']' -f 1 | sed 's/\*//g' | grep -v "/" | sort | uniq -c | sort -n
