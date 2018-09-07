1c284747694e3828410185b84a062f4bd515f8af
MorganGeek <wattiez.morgan@gmail.com>
2018-09-07 12:25:33 +0200

    script: ignore stop words in top_words

--- top_words.sh
+++ top_words.sh
@@ -1,3 +1,3 @@
 #/usr/bin/env bash
 BASEDIR=$(dirname "$0")
-grep -o -E '[a-zA-Z]{3,}' "$BASEDIR/README.md"  | tr A-Z a-z | sort | uniq -c | sort -n
+grep -o -E '[a-zA-Z]{3,}' "$BASEDIR/README.md" | tr A-Z a-z | grep -vwFf stopwords.txt | sort | uniq -c | sort -n
