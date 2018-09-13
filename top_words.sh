435ea0ce4549dd330f291301b7ac41cac5862c5d
Morgan <wattiez.morgan@gmail.com>
2018-09-13 21:03:04 +0200

    script: switch to multiline syntax

--- top_words.sh
+++ top_words.sh
@@ -7,7 +7,18 @@ url="${1:-}"
 basedir=$(dirname "$0")
 
 if [[ "${url:-}" = "" ]]; then
-  grep --only-matching --extended-regexp '[a-zA-Z]{3,}' "$basedir/README.md" | tr '[:upper:]' '[:lower:]' | grep --invert-match --word-regexp --fixed-strings --file=stopwords.txt | sort | uniq --count | sort -n
+  grep --only-matching --extended-regexp '[a-zA-Z]{3,}' "$basedir/README.md" \
+  | tr '[:upper:]' '[:lower:]' \
+  | grep --invert-match --word-regexp --fixed-strings --file=stopwords.txt \
+  | sort \
+  | uniq --count \
+  | sort -n
 else
-  curl "$url" | grep --only-matching --extended-regexp '[a-zA-Z]{3,}' | tr '[:upper:]' '[:lower:]' | grep --invert-match --word-regexp --fixed-strings --file=stopwords.txt | sort | uniq --count | sort -n
+  curl "$url" \
+  | grep --only-matching --extended-regexp '[a-zA-Z]{3,}' \
+  | tr '[:upper:]' '[:lower:]' \
+  | grep --invert-match --word-regexp --fixed-strings --file=stopwords.txt \
+  | sort \
+  | uniq --count \
+  | sort -n
 fi
