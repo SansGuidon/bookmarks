43d38fac70bad28b349444ebc654bb2b5d79be71
Morgan <wattiez.morgan@gmail.com>
2018-09-13 21:14:23 +0200

    script: function style

--- top_words.sh
+++ top_words.sh
@@ -3,22 +3,29 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-url="${1:-}"
-basedir=$(dirname "$0")
+fetch_top_words() {
+  local url
+  local basedir
+  
+  url="${1:-}"
+  basedir=$(dirname "$0")
+  
 
-if [[ "${url:-}" = "" ]]; then
-  grep --only-matching --extended-regexp '[a-zA-Z]{3,}' "$basedir/README.md" \
-  | tr '[:upper:]' '[:lower:]' \
-  | grep --invert-match --word-regexp --fixed-strings --file=stopwords.txt \
-  | sort \
-  | uniq --count \
-  | sort -n
-else
-  curl "$url" \
-  | grep --only-matching --extended-regexp '[a-zA-Z]{3,}' \
-  | tr '[:upper:]' '[:lower:]' \
-  | grep --invert-match --word-regexp --fixed-strings --file=stopwords.txt \
-  | sort \
-  | uniq --count \
-  | sort -n
-fi
+  if [[ "${url:-}" = "" ]]; then
+    grep --only-matching --extended-regexp '[a-zA-Z]{3,}' "$basedir/README.md" \
+    | tr '[:upper:]' '[:lower:]' \
+    | grep --invert-match --word-regexp --fixed-strings --file=stopwords.txt \
+    | sort \
+    | uniq --count \
+    | sort -n
+  else
+    curl "$url" \
+    | grep --only-matching --extended-regexp '[a-zA-Z]{3,}' \
+    | tr '[:upper:]' '[:lower:]' \
+    | grep --invert-match --word-regexp --fixed-strings --file=stopwords.txt \
+    | sort \
+    | uniq --count \
+    | sort -n
+  fi
+}
+fetch_top_words "$@"
