135fc297a3fa67eca9d3b5088a884e509302a03b
Morgan Wattiez <morgan.wattiez@collibra.com>
2019-07-22 13:13:27 +0200

    remove space from command output

--- count_links.sh
+++ count_links.sh
@@ -1,3 +1,3 @@
 #/usr/bin/env bash
 BASEDIR=$(dirname "$0")
-cat "$BASEDIR/README.md" | cut -d'(' -f2 | cut -d')' -f1 | egrep "^http*" | sort -u | wc -l
+cat "$BASEDIR/README.md" | cut -d'(' -f2 | cut -d')' -f1 | egrep "^http*" | sort -u | wc -l | tr -d ' '
