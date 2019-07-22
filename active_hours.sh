06d94dd384060fac20a7ca718177016f4b88d8a7
Morgan Wattiez <morgan.wattiez@collibra.com>
2019-07-22 13:09:06 +0200

    +script to see active working hours

---
+++ active_hours.sh
@@ -0,0 +1,1 @@
+git log --author="Morgan" --date=iso | perl -nalE 'if (/^Date:\s+[\d-]{10}\s(\d{2})/) { say $1+0 }' | sort | uniq -c|perl -MList::Util=max -nalE '$h{$F[1]} = $F[0]; }{ $m = max values %h; foreach (0..23) { $h{$_} = 0 if not exists $h{$_} } foreach (sort {$a <=> $b } keys %h) { say sprintf "%02d - %4d %s", $_, $h{$_}, "*"x ($h{$_} / $m * 50); }'
