e397980803cfa103524ec83abfc6f1a1b0445976
Morgan <morgan.wattiez@collibra.com>
2019-10-12 16:55:38 +0200

    add feature for filtering top results

--- top_authors.sh
+++ top_authors.sh
@@ -1,3 +1,14 @@
-#/usr/bin/env bash
-BASEDIR=$(dirname "$0")
-grep http "$BASEDIR/README.md" | cut -d '[' -f 2 | cut -d ']' -f 1 | sed 's/\*//g' | grep -v "/" | sort | uniq -c | sort -n
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+function fetch_top_authors() {
+  local basedir
+  local filter
+  basedir=$(dirname "$0")
+  filter="${1:-}"
+
+  grep http "$basedir/README.md" | grep "${filter}" | cut -d '[' -f 2 | cut -d ']' -f 1 | sed 's/\*//g' | grep -v "/" | sort | uniq -c | sort -n
+}
+fetch_top_authors "$@"
