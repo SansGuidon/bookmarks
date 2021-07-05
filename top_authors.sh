08b41fbcbe9c80cd1179376e86caf445034776e1
MorganGeek <wattiez.morgan@gmail.com>
2021-07-05 11:08:59 +0200

    fix authors counting for multi files handling

--- top_authors.sh
+++ top_authors.sh
@@ -9,6 +9,6 @@ function fetch_top_authors() {
   basedir=$(dirname "$0")
   filter="${1:-}"
 
-  grep http "$basedir/README.md" | grep -i "${filter}" | cut -d '[' -f 2 | cut -d ']' -f 1 | sed 's/\*//g' | grep -v "/" | sort | uniq -c | sort -n
+  grep http "$basedir/README.md" "$basedir/sections/"*.md | grep -i "${filter}" | cut -d '[' -f 2 | cut -d ']' -f 1 | sed 's/\*//g' | grep -v "/" | sort | uniq -c | sort -n
 }
 fetch_top_authors "$@"
