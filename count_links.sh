ef6ebe381f337730bd76638ecb8c6e4717facea1
MorganGeek <wattiez.morgan@gmail.com>
2021-07-05 11:02:45 +0200

    fix links counting routine for multiple files

--- count_links.sh
+++ count_links.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 BASEDIR=$(dirname "$0")
-cut -d'(' -f2 "$BASEDIR/README.md" | cut -d')' -f1 | grep -E "^http*" | sort -u | wc -l | tr -d ' '
+cut -d'(' -f2 $(pwd)/sections/*.md README.md | cut -d')' -f1 | grep "^http" | sort -u | wc -l | tr -d ' '
